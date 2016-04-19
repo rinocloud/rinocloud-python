ecebe416a1f86e185fb1417c8e74cee004bd5597
eoinmurray <amonecho1@gmail.com>
2016-04-19 17:46:58 +0100

    added requests_toolbest

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
     name="rinocloud",
     version="0.1",
     packages=["rinocloud"],
-	description = 'Rinocloud python bindings',
-	author = 'Eoin Murray',
-	author_email = 'eoin@rinocloud.com',
-	url = 'https://github.com/rinocloud/rinocloud-python', # use the URL to the github repo
-	download_url = 'https://github.com/rinocloud/rinocloud-python/tarball/0.1', # I'll explain this in a second
-	keywords = ['rinocloud', 'data', 'api'], # arbitrary keywords
-	classifiers = [],
+    description='Rinocloud python bindings',
+    author='Eoin Murray',
+    author_email='eoin@rinocloud.com',
+    url='https://github.com/rinocloud/rinocloud-python',  # use the URL to the github repo
+    download_url='https://github.com/rinocloud/rinocloud-python/tarball/0.1',  # I'll explain this in a second
+    keywords=['rinocloud', 'data', 'api'],  # arbitrary keywords
+    classifiers=[],
     install_requires=[
         "requests",
+        "requests_toolbelt",
         "arrow",
         "clint"
     ],
